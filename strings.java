765c96dd455d04e8c9965e8a60bbc20d1f7380f2
Adem Can Demirci <can.demirci2000@gmail.com>
2025-09-11 17:57:18 +0300

    updated

--- strings.java
+++ strings.java
@@ -4,7 +4,11 @@
 public class strings{
     public static void main(String[] args){
         /*String name = "Adem Can"; */ // creating a String using double quotes
-        String name = new String("Adem Can"); // creating a String object using the new keyword
-        System.out.println("Your name is " + name + ".");
+        /*String name = new String("Adem Can"); */ // creating a String object using the new keyword
+        /*System.out.println("Your name is " + name + "."); */
+        String name = "Adem Can";
+        int age = 25;
+        String helloName = String.format("Hello %s!, you are %d years old", name, age);
+        System.out.println(helloName);
     }
 }

2b0cf4fdb01c537954386d73c8fe0fc714c0c4cf
Adem Can Demirci <can.demirci2000@gmail.com>
2025-09-12 15:49:07 +0300

    updated

---
+++ ifElseIf.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ifElseIf {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+        input.close();
+        
+        if (age < 0) {
+            System.out.println("Invalid age.");
+        } else if (age < 18) {
+            System.out.println("You are a minor.");
+        } else if (age < 65) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a senior.");
+        }
+
+        
+    }
+    
+}

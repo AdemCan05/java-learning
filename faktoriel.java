b871c9e4332ea861302c92598fa0c21524ad6bd3
Adem Can Demirci <can.demirci2000@gmail.com>
2025-10-13 11:51:08 +0300

    updated

---
+++ faktoriel.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class faktoriel {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Faktöriyelini almak istediğiniz sayiyi giriniz: ");
+        int n = input.nextInt();
+        input.close();
+
+        int sonuc = 1;
+        for(int i = 1; i<=n; i++){
+            sonuc *= i;
+        }
+        System.out.println(String.format("%d! = %d", n, sonuc));
+    }
+    
+}
